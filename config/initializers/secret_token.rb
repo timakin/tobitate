eddd2f0f210f5b17dcf466fc074f05ca46b992ce
timakin <timaki.st@gmail.com>
2014-03-24 00:45:39 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tobitate::Application.config.secret_key_base = 'e0089f0a261d3c12108dcce9795b00800bfbe836496e43035da37fd8a5097d8aaf0c514746f6dcfed8a708edef4cd6ba251dbbbddbbfa74f8890ea721ed59ea0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tobitate::Application.config.session_store :cookie_store, key: '_tobitate_session'
