b722b4e802ee989f18dae27029e6d361dd19aee9
timakin <timaki.st@gmail.com>
2014-03-24 22:31:19 +0900

    Devise整備

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Tobitate::Application.routes.draw do
+  devise_for :users
   resources :projects
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'projects#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
